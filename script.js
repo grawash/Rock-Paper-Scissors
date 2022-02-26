657668abfc9b8f347cb9979181c24510a0ba080a
zurbash <duta1101@bk.ru>
2022-02-26 16:16:21 +0400

    added playGame function

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function computerPlay(){
     else if (computerPick === 2) return "paper";
     else return "scissors"; 
 }
+let playerSelection;
+let computerSelection;
+function playGame(playerSelection,computerSelection){
+    playerSelection = prompt("your choice ?").toLowerCase();
+    computerSelection = computerPlay().toLowerCase();
+    if (playerSelection === computerSelection) return(playerSelection+" "+computerSelection+" draw");
+    else if(playerSelection==="scissors" && computerSelection==="rock") return("you won! "+playerSelection+" beats "+computerSelection);
+    else if(playerSelection==="rock" && computerSelection==="scissors") return("you won! "+playerSelection+" beats "+computerSelection);
+    else if(playerSelection==="paper" && computerSelection==="rock") return("you won! "+playerSelection+" beats "+computerSelection);
+    else return(playerSelection+" beats "+computerSelection+" you lost to a computer!");
+}
