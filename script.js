1ac9c1fe693dbc021f79412b23083c57224c429e
zurbash <duta1101@bk.ru>
2022-02-26 15:48:14 +0400

    added computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello World!")
+function computerPlay(){
+    let computerPick = Math.floor(Math.random()*3+1);
+    if (computerPick === 1) return "rock";
+    else if (computerPick === 2) return "paper";
+    else return "scissors"; 
+}
