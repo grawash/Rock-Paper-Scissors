caf04d8c5fb70c034f5ec286a7da627988e8cff1
zurbash <duta1101@bk.ru>
2022-03-06 15:02:04 +0400

    added DOM methods for each button, modified script.js so that buttons trigger playGame function

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-console.log("Hello World!")
+const rock = document.querySelector('#rock');
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results = document.querySelector(".results");
+const buttons = document.querySelectorAll('button')
+const gameStatus = document.querySelector(".status");
+
+function disableButtons(){
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
 function computerPlay(){
     let computerPick = Math.floor(Math.random()*3+1);
     if (computerPick === 1) return "rock";
@@ -7,26 +19,20 @@ function computerPlay(){
 }
 let playerSelection;
 let computerSelection;
-function playGame(playerSelection,computerSelection){
-    playerSelection = prompt("your choice ?").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+function playGame(playerSelection){
+ 
     computerSelection = computerPlay().toLowerCase();
-    if (playerSelection === computerSelection) return("draw");
-    else if(playerSelection==="scissors" && computerSelection==="rock") return("you won! "+playerSelection+" beats "+computerSelection);
-    else if(playerSelection==="rock" && computerSelection==="scissors") return("you won! "+playerSelection+" beats "+computerSelection);
-    else if(playerSelection==="paper" && computerSelection==="rock") return("you won! "+playerSelection+" beats "+computerSelection);
-    else return("you lost to a computer!");
+    if (playerSelection === computerSelection){ gameStatus.textContent = "draw";}
+    else if(playerSelection==="scissors" && computerSelection==="rock"){ playerScore++; gameStatus.textContent = ("you won! "+playerSelection+" beats "+computerSelection);}
+    else if(playerSelection==="rock" && computerSelection==="scissors"){ playerScore++; gameStatus.textContent = ("you won! "+playerSelection+" beats "+computerSelection);}
+    else if(playerSelection==="paper" && computerSelection==="rock"){ playerScore++; gameStatus.textContent = ("you won! "+playerSelection+" beats "+computerSelection);}
+    else{ computerScore++; gameStatus.textContent = ("you lost to a computer!")};
+    results.textContent=(`you:${playerScore} computer:${computerScore} `)
+    if(playerScore === 5){ results.textContent="you won"; disableButtons();}
+    if(computerScore === 5){ results.textContent="you lost;"; disableButtons();}
 }
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-    for(let i=0;i<5;i++){
-        console.log("player: "+playerScore+" computer: "+computerScore)
-        result=playGame();
-        if (result==="you lost to a computer!") computerScore++;
-        else if(result==="draw") continue;
-        else playerScore++;
-    }
-    if(playerScore>computerScore) console.log("you won!");
-    else console.log("you lost!");
-}
+rock.addEventListener('click', () => {console.log(playGame("rock"));});
+paper.addEventListener('click', () => {console.log(playGame("paper"));});
+scissors.addEventListener('click',() => {console.log(playGame("scissors"));});
